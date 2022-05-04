c9ce033bcd4b67ab43f51ccdbdedf5069e7dec21
Aya Ramirez <ayaramz@gmail.com>
2022-05-04 00:23:02 -0400

    learn was broken

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    let newTask = document.getElementById('new-task-description')
+    handleToDo(newTask.value)
+    newTask.value = ""
+  })
+})
+
+function handleToDo(todo) {
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  let ul = document.getElementById("tasks")
+
+  li.textContent = todo
+  btn.innerText = ' X '
+  btn.addEventListener('click', (e) => {
+    e.currentTarget.parentElement.remove()
+  })
+  li.append(btn)
+  ul.append(li)
+}
+
+
